e0e9bf56ec6607eb5f2810fd3e13f0af776b57d1
zj05409 <zj05409@qq.com>
2016-09-19 10:47:29 +0800

    add locale set

--- downloadAndPrepareData.R
+++ downloadAndPrepareData.R
@@ -1,3 +1,5 @@
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip","household_power_consumption.zip")
 unzip("household_power_consumption.zip")
 cat(grep("^(Date|1/2/2007|2/2/2007)", readLines("household_power_consumption.txt"), value = TRUE), sep = "\n", file = "household_power_consumption_subset.txt")
+#Sys.setlocale(category = "LC_ALL", locale = "english")
+#shell: defaults write org.R-project.R force.LANG en_US.UTF-8
